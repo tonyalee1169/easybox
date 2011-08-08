82a061ca0225ec1683ef251d03aafc0d935cb52f
vwochnik <v.wochnik@googlemail.com>
2011-08-09 01:31:14 +0200

    Initial commit

---
+++ easybox/autoload.js
@@ -0,0 +1,8 @@
+// AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
+if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry|msie 6/i.test(navigator.userAgent)) {
+	jQuery(function($) {
+		$("a[rel^='lightbox']").easybox({/* Put custom options here */}, null, function(el) {
+			return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
+		});
+	});
+}
---
+++ easybox/easybox.js
@@ -0,0 +1,393 @@
+/*!
+	Easybox v0.1 - Lightweight easy to use lightbox clone for jQuery
+	Based on Slimbox2 by Christophe Beyls <http://www.digitalia.be>
+
+	Copyright (C) 2011 by Vincent Wochnik
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+(function($) {
+	// Global variables
+	var options, resources, activeIndex = -1, prevIndex, nextIndex, centerWidth, centerHeight,
+		hiddenElements = [],
+	// loading requisites
+		imageObj = null, ajaxReq = null,
+	// settings
+		imageWidth = 0, imageHeight = 0, videoWidth = 0, videoHeight = 0, videoWidescreen = 0, loadError = false,
+	// DOM elements
+		overlay, center, container, prevLink, nextLink, bottomContainer, bottom, caption, number;
+
+	/*
+		Initialization
+	*/
+	$(function() {
+		// Append the easybox HTML code at the bottom of the document
+		$("body").append(
+			$([
+				overlay = $('<div id="easyOverlay" />')[0],
+				center = $('<div id="easyCenter" />').append([
+					container = $('<div id="easyContainer" />')[0]
+				])[0],
+				prevLink = $('<a id="easyPrevLink" href="#" />').click(previous)[0],
+				nextLink = $('<a id="easyNextLink" href="#" />').click(next)[0],
+				bottomContainer = $('<div id="easyBottomContainer" />').append([
+					bottom = $('<div id="easyBottom" />').append([
+						$('<a id="easyCloseLink" href="#" />').add(overlay).click(close)[0],
+						caption = $('<div id="easyCaption" />')[0],
+						number = $('<div id="easyNumber" />')[0],
+						$('<div style="clear: both;" />')[0]
+					])[0]
+				])[0]
+			]).css("display", "none")
+		);
+	});
+
+
+	/*
+		API
+		Opens easybox with the specified parameters
+	*/
+	$.easybox = function(_resources, startIndex, _options) {
+		if (activeIndex >= 0)
+			return false;
+
+		// complete options
+		options = $.extend({
+			loop: false,				// Allows to navigate between first and last images
+			overlayOpacity: 0.8,			// 1 is opaque, 0 is completely transparent (change the color in the CSS file)
+			resizeDuration: 400,			// Duration of each of the box resize animations (in milliseconds)
+			fadeDuration: 400,			// Duration of the image fade-in animation (in milliseconds)
+			resizeEasing: "swing",			// "swing" is jQuery's default easing
+			initialWidth: 250,			// Initial width of the box (in pixels)
+			initialHeight: 250,			// Initial height of the box (in pixels)
+			closeWidth: 128,
+			closeHeight: 128,
+			contentWidth: 640,			// default content width
+			contentHeight: 480,			// default content height
+			ytPlayerHeight: 360,		// default youtube player height
+			captionAnimationDuration: 400,		// Duration of the caption animation (in milliseconds)
+			counterText: "{x} of {y}",	// Translate or change as you wish, or set it to false to disable counter text for image groups
+			closeKeys: [27, 88, 67],		// Array of keycodes to close easybox, default: Esc (27), 'x' (88), 'c' (67)
+			previousKeys: [37, 80],			// Array of keycodes to navigate to the previous image, default: Left arrow (37), 'p' (80)
+			nextKeys: [39, 78]			// Array of keycodes to navigate to the next image, default: Right arrow (39), 'n' (78)
+		}, _options);
+
+		// The function is called for a single image, with URL and Title as first two arguments
+		if (typeof _resources == "string") {
+			_resources = [[_resources, startIndex]];
+			startIndex = 0;
+		}
+
+		// copy resources array and set loop option
+		resources = _resources;
+		options.loop = options.loop && (resources.length > 1);
+
+		// initializing center
+		centerWidth = options.initialWidth;
+		centerHeight = options.initialHeight;
+		$(center).css({width: centerWidth, height: centerHeight, marginLeft: -centerWidth/2, marginTop: -centerHeight/2, opacity: ""});
+
+		setup(1);
+		$(center).show();
+		$(overlay).css("opacity", options.overlayOpacity).fadeIn(options.fadeDuration, function() {
+			change(startIndex);
+		});
+
+		return false;
+	};
+
+	/*
+		options:	Optional options object, see jQuery.easybox()
+		linkMapper:	Optional function taking a link DOM element and an index as arguments and returning an array containing 2 elements:
+				the image URL and the image caption (may contain HTML)
+		linksFilter:	Optional function taking a link DOM element and an index as arguments and returning true if the element is part of
+				the image collection that will be shown on click, false if not. "this" refers to the element that was clicked.
+				This function must always return true when the DOM element argument is "this".
+	*/
+	$.fn.easybox = function(_options, linkMapper, linksFilter) {
+		linkMapper = linkMapper || function(el) {
+			return [el.href, el.title];
+		};
+
+		linksFilter = linksFilter || function() {
+			return true;
+		};
+
+		var links = this;
+
+		return links.unbind("click").click(function() {
+			// Build the list of resources that will be displayed
+			var link = this, startIndex = 0, filteredLinks, i = 0, length;
+			filteredLinks = $.grep(links, function(el, i) {
+				return linksFilter.call(link, el, i);
+			});
+
+			// We cannot use jQuery.map() because it flattens the returned array
+			for (length = filteredLinks.length; i < length; ++i) {
+				if (filteredLinks[i] == link) startIndex = i;
+				filteredLinks[i] = linkMapper(filteredLinks[i], i);
+			}
+			return $.easybox(filteredLinks, startIndex, _options);
+		});
+	};
+
+
+	/*
+		Setup and unsetup function
+	*/
+	function setup(open) {
+		if (open) {
+			$("object").add("embed").each(function(index, el) {
+				hiddenElements[index] = [el, el.style.visibility];
+				el.style.visibility = "hidden";
+			});
+		} else {
+			$.each(hiddenElements, function(index, el) {
+				el[0].style.visibility = el[1];
+			});
+			hiddenElements = [];
+		}
+		var fn = open ? "bind" : "unbind";
+		$(document)[fn]("keydown", keyDown);
+	}
+
+	/*
+		Key handling function
+	*/
+	function keyDown(event) {
+		var code = event.keyCode, fn = $.inArray;
+		// Prevent default keyboard action (like navigating inside the page)
+		return (fn(code, options.closeKeys) >= 0) ? close()
+			: (fn(code, options.nextKeys) >= 0) ? next()
+			: (fn(code, options.previousKeys) >= 0) ? previous()
+			: false;
+	}
+
+	/*
+		Jump to previous resource
+	*/
+	function previous() {
+		return change(prevIndex);
+	}
+
+	/*
+		Jump to next resource
+	*/
+	function next() {
+		return change(nextIndex);
+	}
+
+	/*
+		Change resource
+	*/
+	function change(index) {
+		if (index >= 0) {
+			activeIndex = index;
+			prevIndex = (activeIndex || (options.loop ? resources.length : 0)) - 1;
+			nextIndex = ((activeIndex + 1) % resources.length) || (options.loop ? 0 : -1);
+			
+			// reset everything
+			stop();
+			
+			// preload previous and next image
+			if ((prevIndex >= 0) && (/(\.jpg|\.jpeg|\.png|\.gif)$/i.test(resources[prevIndex][0])))
+				(new Image()).src = resources[prevIndex][0];
+			if ((nextIndex >= 0) && (/(\.jpg|\.jpeg|\.png|\.gif)$/i.test(resources[nextIndex][0])))
+				(new Image()).src = resources[nextIndex][0];
+
+			if (/(\.jpg|\.jpeg|\.png|\.gif)$/i.test(resources[activeIndex][0])) {
+				$(center).addClass("easyLoading");
+				imageObj = new Image();
+				imageObj.onload = function() {
+					imageWidth = this.width;
+					imageHeight = this.height;
+					animateBox();
+				};
+				imageObj.onerror = function() {
+					loadError = true;
+					animateBox();
+				}
+				imageObj.src = resources[activeIndex][0];
+			} else if ((r = /^http\:\/\/www\.youtube\.com\/watch\?v=([A-Za-z0-9]*)(&(.*))?/i.exec(resources[activeIndex][0])) != null) {
+				$(center).addClass("easyLoading");
+				ajaxReq = $.ajax('http://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc', {
+					type: 'GET',
+					data: {q: r[1]},
+					dataType: 'jsonp',
+					timeout: 2000,
+					success: function(r) {
+						if (r.data.totalItems) {
+							if (r.data.items[0].accessControl.embed == "allowed")
+								videoWidescreen = (r.data.items[0].aspectRatio == "widescreen");
+							else
+								loadError = true;
+						}
+						animateBox();
+					},
+					error: function(x, t) {
+						if (t != "abort") {
+							loadError = true;
+							animateBox();
+						}
+					}});
+			} else if ((r = /^http\:\/\/vimeo\.com\/([0-9]*)(.*)?/i.exec(resources[activeIndex][0])) != null) {
+				$(center).addClass("easyLoading");
+				ajaxReq = $.ajax('http://vimeo.com/api/v2/video/'+r[1]+'.json', {
+					type: 'GET',
+					dataType: 'jsonp',
+					timeout: 2000,
+					success: function(r) {
+						if (r.length) {
+							if ((r[0].embed_privacy == 'anywhere') || (r[0].embed_privacy == 'approved')) {
+								videoWidth = r[0].width || 0;
+								videoHeight = r[0].height || 0;
+							} else {
+								loadError = true;
+							}
+						}
+						animateBox();
+					},
+					error: function(x, t) {
+						if (t != "abort") {
+							loadError = true;
+							animateBox();
+						}
+					}});
+			} else {
+				// don't need preloading
+				animateBox();
+			}
+		}
+
+		return false;
+	}
+
+	/*
+		Animates the box
+		Called by change()
+	*/
+	function animateBox() {
+		var cw = 0, ch = 0;
+		//var t = $(window).scrollTop(), h = $(window).height();
+
+		// remove loading animation
+		$(center).removeClass();
+		
+		if (!loadError) {
+			if (/(\.jpg|\.jpeg|\.png|\.gif)$/i.test(resources[activeIndex][0])) {
+				cw = (imageWidth > 0) ? imageWidth : options.contentWidth;
+				ch = (imageHeight > 0) ? imageHeight : options.contentHeight;
+				$("<img src=\""+resources[activeIndex][0]+"\" width=\""+cw+"\" height=\""+ch+"\" alt=\""+resources[activeIndex][1]+"\" />").appendTo(container);
+			} else if ((r = /^http\:\/\/www\.youtube\.com\/watch\?v=([A-Za-z0-9]*)(&(.*))?/i.exec(resources[activeIndex][0])) != null) {
+				ch = options.ytPlayerHeight;
+				if (videoWidescreen)
+					cw = Math.round(ch*16.0/9.0);
+				else
+					cw = Math.round(ch*4.0/3.0);
+				$("<object style=\"width:"+cw+"px;height:"+ch+"px\" width=\""+cw+"\" height=\""+ch+"\"><param name=\"movie\" value=\"http://www.youtube.com/v/"+r[1]+"?version=3&autohide=1&autoplay=1&rel=0\"></param><param name=\"AllowFullscreen\" value=\"true\"></param><param name=\"AllowScriptAccess\" value=\"always\"></param></param><embed src=\"http://www.youtube.com/v/"+r[1]+"?version=3&autohide=1&autoplay=1&rel=0\" width=\""+cw+"\" height=\""+ch+"\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\"></embed></object>").appendTo(container);
+			} else if ((r = /^http\:\/\/vimeo\.com\/([0-9]*)(.*)?/i.exec(resources[activeIndex][0])) != null) {
+				cw = (videoWidth > 0) ? videoWidth : options.contentWidth;
+				ch = (videoHeight > 0) ? videoHeight : options.contentHeight;
+				$("<iframe src=\"http://player.vimeo.com/video/"+r[1]+"?title=0&byline=0&portrait=0&autoplay=true\" width=\""+cw+"\" height=\""+ch+"\" frameborder=\"0\"></iframe>").appendTo(container);
+			} else {
+				cw = options.contentWidth;
+				ch = options.contentHeight;
+				$("<iframe width=\""+cw+"\" height=\""+ch+"\" src=\""+resources[activeIndex][0]+"\"></iframe>").appendTo(container);
+			}
+			
+			// retrieve center dimensions
+			$(container).css({visibility: "hidden", display: ""}).width(cw).height(ch);
+			centerWidth = container.offsetWidth;
+			centerHeight = container.offsetHeight;
+
+			// set caption
+			$(caption).html(resources[activeIndex][1] || "");
+		} else {
+			$(center).addClass("easyError");
+			centerWidth = options.initialWidth;
+			centerHeight = options.initialHeight;
+
+			// clear caption
+			$(caption).html("");
+		}
+
+		// set number
+		$(number).html((((resources.length > 1) && options.counterText) || "").replace(/{x}/, activeIndex + 1).replace(/{y}/, resources.length));
+
+		// resize center
+		if ((center.offsetHeight != centerHeight) || (center.offsetWidth != centerWidth))
+			$(center).animate({height: centerHeight, marginTop: -centerHeight/2, width: centerWidth, marginLeft: -centerWidth/2}, options.resizeDuration, options.resizeEasing);
+
+		$(center).queue(function() {
+			$(bottomContainer).css({width: centerWidth, marginLeft: -centerWidth/2, marginTop: centerHeight/2});
+			$(prevLink).css({marginLeft: -centerWidth/2 - Math.floor($(prevLink).width() * 1.5)});
+			$(nextLink).css({marginLeft: centerWidth/2 + Math.ceil($(prevLink).width() * 0.5)});
+			$(container).css({display: "none", visibility: "", opacity: ""}).fadeIn(options.fadeDuration, animateCaption);
+		});
+	}
+
+	/*
+		Animates the caption
+		Called by animateBox() when finished
+	*/
+	function animateCaption() {
+		if (prevIndex >= 0) $(prevLink).css({display: "none", visibility: "", opacity: ""}).fadeIn(options.captionAnimationDuration);
+		if (nextIndex >= 0) $(nextLink).css({display: "none", visibility: "", opacity: ""}).fadeIn(options.captionAnimationDuration);
+
+		// fade in		
+		$(bottomContainer).css({visibility: "", display: ""});
+		$(bottom).css("marginTop", -bottom.offsetHeight).animate({marginTop: 0}, options.captionAnimationDuration);
+	}
+
+	/*
+		Stops all animation and resets the box to a clear state
+		Called by close() and change()
+	*/
+	function stop() {
+		// reset everything to init state
+		$(center).removeClass();
+		if (imageObj != null) { imageObj.onload = imageObj.onerror = null; imageObj = null; }
+		if (ajaxReq != null) { ajaxReq.abort(); ajaxReq = null; }		
+		videoWidescreen = loadError = false;
+		imageWidth = imageHeight = 0;
+		videoWidth = videoHeight = 0;
+		$(container).empty();
+		$([center, container, bottom, prevLink, nextLink]).stop(true);
+		$([container, bottomContainer, prevLink, nextLink]).css({display: "none", visibility: "hidden"});
+	}
+
+	/*
+		Closes the box
+	*/
+	function close() {
+		if (activeIndex >= 0) {
+			stop();
+			activeIndex = prevIndex = nextIndex = -1;
+			// resize center
+			$(overlay).stop().fadeOut(options.fadeDuration, setup);
+			$(center).animate({height: options.closeHeight, marginTop: -options.closeHeight/2, width: options.closeWidth, marginLeft: -options.closeWidth/2, opacity: 0}, options.fadeDuration, options.resizeEasing, function() {
+				$(center).hide();
+			});
+		}
+
+		return false;
+	}
+
+})(jQuery);
