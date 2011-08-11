6cf05739a905d80c59c5379e657bf5bf785e372d
vwochnik <v.wochnik@googlemail.com>
2011-08-12 00:43:41 +0200

    Added slideshow rel

--- easybox/autoload.js
+++ easybox/autoload.js
@@ -4,5 +4,8 @@ if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry|msie 6/i.test(n
 		$("a[rel^='lightbox']").easybox({/* Put custom options here */}, null, function(el) {
 			return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
 		});
+		$("a[rel^='slideshow']").easybox({slideshow: 5000}, null, function(el) {
+			return (this == el) || ((this.rel.length > 9) && (this.rel == el.rel));
+		});
 	});
 }
--- easybox/easybox.js
+++ easybox/easybox.js
@@ -23,6 +23,17 @@
 	THE SOFTWARE.
 */
 
+// AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
+if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry|msie 6/i.test(navigator.userAgent)) {
+	jQuery(function($) {
+		$("a[rel^='lightbox']").easybox({/* Put custom options here */}, null, function(el) {
+			return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
+		});
+		$("a[rel^='slideshow']").easybox({slideshow: 5000}, null, function(el) {
+			return (this == el) || ((this.rel.length > 9) && (this.rel == el.rel));
+		});
+	});
+}
 (function($) {
 	// Global variables
 	var options, resources, activeIndex = -1, prevIndex, nextIndex, centerWidth, centerHeight,
